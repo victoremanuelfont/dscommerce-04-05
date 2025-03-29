0449378490e26ae5b044584c99521946b947a56b
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-03-29 09:15:31 -0300

    Restringir para somente ADMIN fazer a ação

--- src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscommerce.dto.OrderDTO;
 import com.devsuperior.dscommerce.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class OrderController {
     @Autowired
     private OrderService service;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO dto = service.findById(id);
